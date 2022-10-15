824e8d966246287a7d937626afaafbfe106e6087
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-10-15 07:01:40 +0200

    test 3.1

--- MK10-Armour/prototypes/item.lua
+++ MK10-Armour/prototypes/item.lua
@@ -31,7 +31,7 @@ data:extend({
     order = "c[modular-armor]",
     stack_size = 1,
     infinite = true,
-    equipment_grid = "larg-equipment-grid",
+    equipment_grid = "medium-equipment-grid",
     inventory_size_bonus = 10,
     open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
     close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
