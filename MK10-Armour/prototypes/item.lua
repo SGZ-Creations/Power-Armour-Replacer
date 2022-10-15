5db8d82b6819b04070c39128a98430e18f06db7e
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-10-15 06:46:16 +0200

    Item.lua

---
+++ MK10-Armour/prototypes/item.lua
@@ -0,0 +1,372 @@
+data:extend({
+    {
+    type = "armor",
+    name = "a_mk1",
+    icon = "__base__/graphics/icons/power-armor.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 6,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 50
+      },
+      {
+        type = "explosion",
+        decrease = 30,
+        percent = 35
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 40
+      }
+    },
+    subgroup = "armor",
+    order = "c[modular-armor]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 10,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk2",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk3",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk4",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk5",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk6",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk7",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk8",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk9",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+  {
+    type = "armor",
+    name = "a_mk10",
+    icon = "__base__/graphics/icons/power-armor-mk2.png",
+    icon_size = 64, icon_mipmaps = 4,
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 8,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 0,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 40
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 60
+      }
+    },
+    subgroup = "armor",
+    order = "d[power-armor-mk2]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "larg-equipment-grid",
+    inventory_size_bonus = 20,
+    open_sound = {filename =  "__base__/sound/armor-open.ogg", volume = 1},
+    close_sound = {filename = "__base__/sound/armor-close.ogg", volume = 1}
+  },
+})
