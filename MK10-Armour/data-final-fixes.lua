3b2dde9e926f4b3b7b93331c91575abbb78598f5
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-10-20 02:19:44 +0200

    Dissabling vanilla items

--- MK10-Armour/data-final-fixes.lua
+++ MK10-Armour/data-final-fixes.lua
@@ -1,6 +1,20 @@
+--armours
 data.raw.recipe["modular-armor"].enabled = false
 data.raw.technology["modular-armor"].enabled = false
 data.raw.recipe["power-armor"].enabled = false
 data.raw.technology["power-armor"].enabled = false
 data.raw.recipe["power-armor-mk2"].enabled = false
 data.raw.technology["power-armor-mk2"].enabled = false
+--equipment
+data.raw.recipe["energy-shield-equipment"].enabled = false
+data.raw.technology["energy-shield-equipment"].enabled = false
+data.raw.recipe["energy-shield-mk2-equipment"].enabled = false
+data.raw.technology["energy-shield-mk2-equipment"].enabled = false
+data.raw.recipe["fusion-reactor-equipment"].enabled = false
+data.raw.technology["fusion-reactor-equipment"].enabled = false
+data.raw.recipe["night-vision-equipment"].enabled = false
+data.raw.technology["night-vision-equipment"].enabled = false
+data.raw.recipe["battery-equipment"].enabled = false
+data.raw.technology["battery-equipment"].enabled = false
+data.raw.recipe["battery-mk2-equipment"].enabled = false
+data.raw.technology["battery-mk2-equipment"].enabled = false
