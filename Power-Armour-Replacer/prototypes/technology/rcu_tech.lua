e9c582dc88c0afccfdd0308d1ef09f9683de991d
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2023-12-11 11:16:32 +0100

    Texure fix

--- Power-Armour-Replacer/prototypes/technology/rcu_tech.lua
+++ Power-Armour-Replacer/prototypes/technology/rcu_tech.lua
@@ -4,7 +4,7 @@ if not data.raw.technology["rocket-control-unit"] then
 			type = "technology",
 			name = "rocket-control-unit",
 			icon_size = 256, icon_mipmaps = 4,
-			icon = "__Power-Armour-Replacer__/texture/tech/rocket-control-unit.png",
+			icon = "__Power-Armour-Replacer__/texture/tech/rcu.png",
 			effects = {
 				type = "unlock-recipe",
 				recipe = "rocket-control-unit"
