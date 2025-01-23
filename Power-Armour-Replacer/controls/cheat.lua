e2e8314dfb9a2395d567e12f0acb5f177dd55014
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-01-23 17:01:16 +0100

    trying to use but has broken in 2.0

--- Power-Armour-Replacer/controls/cheat.lua
+++ Power-Armour-Replacer/controls/cheat.lua
@@ -3,9 +3,8 @@ commands.add_command("give_me_one", "Gives 1 item", function(event)
     local player_index = event.player_index
     local player = game.get_player(player_index)
     local parameter = event.parameter
-    if game.item_prototypes[parameter] then
+    if game.give[parameter] then
         player.insert{name = parameter, count = 1}
     end
-end
-)
+end)
 ]]
