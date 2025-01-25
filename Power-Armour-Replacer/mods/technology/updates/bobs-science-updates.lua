aee42e406b6afa88372f102921fd841cb7c1c0f6
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-01-25 19:36:36 +0100

    load location. Changelog

--- Power-Armour-Replacer/mods/technology/updates/bobs-science-updates.lua
+++ Power-Armour-Replacer/mods/technology/updates/bobs-science-updates.lua
@@ -45,3 +45,17 @@ if mods["bobtech"] then
 		end
 	end
 end
+
+if mods["bobtech"] and mods["boblogistics"] then
+	table.insert(data.raw.technology["par-roboport-tech-5"].unit.ingredients, {"advanced-logistic-science-pack", 1})
+
+	table.insert(data.raw.technology["par-roboport-tech-6"].unit.ingredients, {"advanced-logistic-science-pack", 2})
+
+	table.insert(data.raw.technology["par-roboport-tech-7"].unit.ingredients, {"advanced-logistic-science-pack", 3})
+
+	table.insert(data.raw.technology["par-roboport-tech-8"].unit.ingredients, {"advanced-logistic-science-pack", 4})
+
+	table.insert(data.raw.technology["par-roboport-tech-9"].unit.ingredients, {"advanced-logistic-science-pack", 5})
+
+	table.insert(data.raw.technology["par-roboport-tech-10"].unit.ingredients, {"advanced-logistic-science-pack", 6})
+end
