eedb524d310e4a2aed7c3c5e7378fce73d9be1fd
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-02-01 10:47:22 +0100

    internal command

--- Power-Armour-Replacer/controls/cheat.lua
+++ Power-Armour-Replacer/controls/cheat.lua
@@ -1,5 +1,5 @@
 --[[
-commands.add_command("give_me_one", "Gives 1 item", function(event)
+commands.add_command("give", "Gives item", function(event)
     local player_index = event.player_index
     local player = game.get_player(player_index)
     local parameter = event.parameter
