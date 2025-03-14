18643cd6bc65eeb74f9529aa7c025c284fc97801
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-03-14 17:28:09 +0100

    ACU Recipe 2

--- Power-Armour-Replacer/prototypes/items/acu_item.lua
+++ Power-Armour-Replacer/prototypes/items/acu_item.lua
@@ -1,14 +1,12 @@
-if not data.raw.item["rocket-control-unit"] then
-	data:extend({
-		{
-			type = "item",
-			name = "armour-control-unit",
-			icon = "__Power-Armour-Replacer__/graphics/icons/acu/acu-icon.png",
-			icon_size = 64,
-			subgroup = "intermediate-product",
-			order = "n[armour-control-unit]",
-			stack_size = 500,
-			weight = 2500,
-		},
-	})
-end
+data:extend({
+	{
+		type = "item",
+		name = "armour-control-unit",
+		icon = "__Power-Armour-Replacer__/graphics/icons/acu/acu-icon.png",
+		icon_size = 64,
+		subgroup = "intermediate-product",
+		order = "n[armour-control-unit]",
+		stack_size = 500,
+		weight = 2500,
+	},
+})
