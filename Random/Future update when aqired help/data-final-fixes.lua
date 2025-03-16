a5c4082fe29ad8acda9676629f1bbac7131d8389
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-03-16 12:21:44 +0100

    Removed folder

--- Random/Future update when aqired help/data-final-fixes.lua
+++
@@ -1,9 +0,0 @@
---structures
-	require("structures.dependency-final")
-	require("structures.filters")
-
---compatibilities.misc load order dependant
-	require("compatibilities.mods-recipes.compatibility-recipe-final")
-	require("compatibilities.misc.altered-tech-final")
-
---compatibilities.tech load order dependant
