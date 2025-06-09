0f79f1608eb265507b85a8a87e7fc865b1c46197
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-06-09 14:18:53 +0200

    Migrations that breaks the Fucked simulastions.

---
+++ Power-Armour-Replacer/structures/SimulationScenarioCrash.lua
@@ -0,0 +1,10 @@
+--(Fuck Factorio updates) ScenearioSimulations crashing due to fucking migrations
+local simulation_crash = {
+    "menu-simulation-nauvis-tank-building",
+}
+    -- "dontcare" replace with "_" 
+    local default_utility_constants = data.raw["utility-constants"].default
+    local menu_simulations = default_utility_constants.main_menu_simulations
+for _, simulation_name in pairs(simulation_crash) do
+    menu_simulations[simulation_name] = nil
+end
