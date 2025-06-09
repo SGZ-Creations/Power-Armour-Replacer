5666642902c9b91d3319e4bebe8ac7d3ece03981
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-06-09 09:58:35 +0200

    SpaceAge addon

--- Power-Armour-Replacer/mods/technology/updates/bobs-science-updates.lua
+++ Power-Armour-Replacer/mods/technology/updates/bobs-science-updates.lua
@@ -1,61 +1,63 @@
 ---@class data.TechnologyPrototype.type
 local Tech = data.raw["technology"]
+---@settings data.settings
+local SS = settings.startup
 
 if mods["bobtech"]and mods["bobplates"] then
 	Tech["steel-processing"].research_trigger = {type ="craft-fluid", fluid ="bob-oxygen", amount = 1000}
 	--Tech["steel-processing"].unit = nil
 
-	data.raw.technology["bob-electrolysis-1"].research_trigger = {type ="craft-item", item ="boiler", count = 12}
-	data.raw.technology["bob-electrolysis-1"].unit = nil
+	Tech["bob-electrolysis-1"].research_trigger = {type ="craft-item", item ="boiler", count = 12}
+	Tech["bob-electrolysis-1"].unit = nil
 
-	data.raw.technology["bob-chemical-processing-1"].research_trigger = {type ="craft-item", item ="steam-engine", count = 24}
-	data.raw.technology["bob-chemical-processing-1"].unit = nil
+	Tech["bob-chemical-processing-1"].research_trigger = {type ="craft-item", item ="steam-engine", count = 24}
+	Tech["bob-chemical-processing-1"].unit = nil
 end
 
 if mods["bobtech"] then
-	if settings.startup["bobmods-burnerphase"].value == true then
+	if SS["bobmods-burnerphase"].value == true then
 		if mods["bobplates"]then
-			data.raw.technology["bob-alloy-processing"].research_trigger = {type ="craft-item", item ="bob-tin-plate", count = 50}
-			data.raw.technology["bob-alloy-processing"].unit = nil
+			Tech["bob-alloy-processing"].research_trigger = {type ="craft-item", item ="bob-tin-plate", count = 50}
+			Tech["bob-alloy-processing"].unit = nil
 
-			data.raw.technology["logistics"].research_trigger = {type ="craft-item", item ="inserter", count = 20}
-			data.raw.technology["logistics"].unit = nil
+			Tech["logistics"].research_trigger = {type ="craft-item", item ="inserter", count = 20}
+			Tech["logistics"].unit = nil
 
-			data.raw.technology["bob-lab"].research_trigger = {type ="craft-item", item ="transport-belt", count = 20}
-			data.raw.technology["bob-lab"].unit = nil
+			Tech["bob-lab"].research_trigger = {type ="craft-item", item ="transport-belt", count = 20}
+			Tech["bob-lab"].unit = nil
 
-			data.raw.technology["logistic-science-pack"].research_trigger = {type ="craft-item", item ="lab", count = 10}
-			data.raw.technology["logistic-science-pack"].unit = nil
+			Tech["logistic-science-pack"].research_trigger = {type ="craft-item", item ="lab", count = 10}
+			Tech["logistic-science-pack"].unit = nil
 
 			if mods["bobelectronics"]then
-				data.raw.technology["electronics"].research_trigger = {type ="craft-item", item ="small-electric-pole", count = 10}
-				data.raw.technology["electronics"].unit = nil
+				Tech["electronics"].research_trigger = {type ="craft-item", item ="small-electric-pole", count = 10}
+				Tech["electronics"].unit = nil
 
-				data.raw.technology["bob-electrolysis-1"].research_trigger = {type ="craft-item", item ="bob-basic-circuit-board", count = 50}
-				data.raw.technology["bob-electrolysis-1"].unit = nil
+				Tech["bob-electrolysis-1"].research_trigger = {type ="craft-item", item ="bob-basic-circuit-board", count = 50}
+				Tech["bob-electrolysis-1"].unit = nil
 
-				data.raw.technology["bob-chemical-processing-1"].research_trigger = {type ="craft-item", item ="bob-carbon", count = 25}
-				data.raw.technology["bob-chemical-processing-1"].unit = nil
+				Tech["bob-chemical-processing-1"].research_trigger = {type ="craft-item", item ="bob-carbon", count = 25}
+				Tech["bob-chemical-processing-1"].unit = nil
 			end
 		end
 	end
 end
 
 if mods["bobtech"] and mods["boblogistics"] then
-	table.insert(data.raw.technology["par-roboport-tech-5"].unit.ingredients, {"bob-advanced-logistic-science-pack", 1})
+	table.insert(Tech["par-roboport-tech-5"].unit.ingredients, {"bob-advanced-logistic-science-pack", 1})
 
-	table.insert(data.raw.technology["par-roboport-tech-6"].unit.ingredients, {"bob-advanced-logistic-science-pack", 2})
+	table.insert(Tech["par-roboport-tech-6"].unit.ingredients, {"bob-advanced-logistic-science-pack", 2})
 
-	table.insert(data.raw.technology["par-roboport-tech-7"].unit.ingredients, {"bob-advanced-logistic-science-pack", 3})
+	table.insert(Tech["par-roboport-tech-7"].unit.ingredients, {"bob-advanced-logistic-science-pack", 3})
 
-	table.insert(data.raw.technology["par-roboport-tech-8"].unit.ingredients, {"bob-advanced-logistic-science-pack", 4})
+	table.insert(Tech["par-roboport-tech-8"].unit.ingredients, {"bob-advanced-logistic-science-pack", 4})
 
-	table.insert(data.raw.technology["par-roboport-tech-9"].unit.ingredients, {"bob-advanced-logistic-science-pack", 5})
+	table.insert(Tech["par-roboport-tech-9"].unit.ingredients, {"bob-advanced-logistic-science-pack", 5})
 
-	table.insert(data.raw.technology["par-roboport-tech-10"].unit.ingredients, {"bob-advanced-logistic-science-pack", 6})
+	table.insert(Tech["par-roboport-tech-10"].unit.ingredients, {"bob-advanced-logistic-science-pack", 6})
 end
 
 if not mods["boblibrary"] then
-    local tech_eff = data.raw.technology["par-armour-tech-1"].effects
+    local tech_eff = Tech["par-armour-tech-1"].effects
     table.insert(tech_eff,{type="unlock-recipe",recipe="iron-stick"})
 end
