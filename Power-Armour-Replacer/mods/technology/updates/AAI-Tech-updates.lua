c95bcf89e5f087c81fbcb066738890f40faf7bfe
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-06-24 11:50:59 +0200

    fixed AAI not having the correct research_trigger

--- Power-Armour-Replacer/mods/technology/updates/AAI-Tech-updates.lua
+++ Power-Armour-Replacer/mods/technology/updates/AAI-Tech-updates.lua
@@ -3,10 +3,10 @@ local Tech = data.raw["technology"]
 
 if mods["aai-industry"]and mods["bobplates"] then
 
-	Tech["sand-processing"].research_trigger = {type ="craft-item", item ="boiler", count = 12}
+	Tech["sand-processing"].research_trigger = {type ="craft-item", item ="motor", count = 50}
 	Tech["sand-processing"].unit = nil
 
-	Tech["glass-processing"].research_trigger = {type ="craft-item", item ="steam-engine", count = 24}
+	Tech["glass-processing"].research_trigger = {type ="craft-item", item ="sand", count = 100}
 	Tech["glass-processing"].unit = nil
 
 	Tech["lamp"].research_trigger = {type ="craft-item", item ="glass", count = 50}
