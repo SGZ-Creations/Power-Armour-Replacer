0c30257dcd92befdf8b23a0dad348091b2a87cee
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-06-30 13:28:16 +0200

    FuckRecycling

--- Power-Armour-Replacer/structures/FalseRecycling.lua
+++ Power-Armour-Replacer/structures/FalseRecycling.lua
@@ -1,25 +1,25 @@
 ---@class data.RecipePtototype
 local Recipe = data.raw.recipe
----@class data.RecipePtototype
-local Item = data.raw.item
+---@class data.ArmorPrototype
+local Armour = data.raw.armor
 
 if mods["quality"] then
 	Recipe["modular-armor"].auto_recycle = false
 	Recipe["power-armor"].auto_recycle = false
-	Item["modular-armor"].auto_recycle = false
-	Item["power-armor"].auto_recycle = false
+	Armour["modular-armor"].auto_recycle = false
+	Armour["power-armor"].auto_recycle = false
 end
 
-Recipe["par-armour-mk1"].auto_recycle = false
-Recipe["par-armour-mk2"].auto_recycle = false
-Recipe["par-armour-mk3"].auto_recycle = false
-Recipe["par-armour-mk4"].auto_recycle = false
-Recipe["par-armour-mk5"].auto_recycle = false
-Recipe["par-armour-mk6"].auto_recycle = false
-Recipe["par-armour-mk7"].auto_recycle = false
-Recipe["par-armour-mk8"].auto_recycle = false
-Recipe["par-armour-mk9"].auto_recycle = false
-Recipe["par-armour-mk10"].auto_recycle = false
+Recipe["par-armour-mk1"].auto_recycle = false Armour["par-armour-mk1"].auto_recycle = false
+Recipe["par-armour-mk2"].auto_recycle = false Armour["par-armour-mk2"].auto_recycle = false
+Recipe["par-armour-mk3"].auto_recycle = false Armour["par-armour-mk3"].auto_recycle = false
+Recipe["par-armour-mk4"].auto_recycle = false Armour["par-armour-mk4"].auto_recycle = false
+Recipe["par-armour-mk5"].auto_recycle = false Armour["par-armour-mk5"].auto_recycle = false
+Recipe["par-armour-mk6"].auto_recycle = false Armour["par-armour-mk6"].auto_recycle = false
+Recipe["par-armour-mk7"].auto_recycle = false Armour["par-armour-mk7"].auto_recycle = false
+Recipe["par-armour-mk8"].auto_recycle = false Armour["par-armour-mk8"].auto_recycle = false
+Recipe["par-armour-mk9"].auto_recycle = false Armour["par-armour-mk9"].auto_recycle = false
+Recipe["par-armour-mk10"].auto_recycle = false Armour["par-armour-mk10"].auto_recycle = false
 --[[
 Recipe[""].auto_recycle = false
 Recipe[""].auto_recycle = false
