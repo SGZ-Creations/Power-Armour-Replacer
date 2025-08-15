948060138b85121d64fef76faa69c8024348e30d
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-15 17:14:49 +0200

    0.3.23-0

--- Power-Armour-Replacer/mods/recipes/updates/compatible_updates.lua
+++ Power-Armour-Replacer/mods/recipes/updates/compatible_updates.lua
@@ -27,6 +27,50 @@ if mods["RampantArsenalFork"] and mods["space-age"] then
     }
 end
 
+if mods["space-age"] then
+    Armour["mech-armor"].resistances = {
+        {
+            type = "physical",
+            decrease = 950,
+            percent = 90,
+        },
+        {
+            type = "acid",
+            decrease = 90,
+            percent = 8.
+        },
+        {
+            type = "explosion",
+            decrease = 10,
+            percent = 10,
+        },
+        {
+            type = "fire",
+            decrease = 0,
+            percent = 100
+        },
+        {
+            type = "electric",
+            decrease = 0,
+            percent = 250,
+        },
+        {
+            type = "impact",
+            decrease = 0,
+            percent = 100,
+        },
+        {
+            type = "laser",
+            decrease = 0,
+            percent = 100,
+        },
+        {
+            type = "poison",
+            decrease = 50,
+            percent = -200,
+        },
+    }
+end
 if mods["RampantArsenalFork"] and mods["space-age"] then
     Armour["combat-mech-armor-rampant-arsenal"].resistances = {
         {
