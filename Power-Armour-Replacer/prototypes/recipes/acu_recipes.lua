d83be4745d8210809a88b56da6ef9ac9ebe1d1d5
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-29 16:22:06 +0200

    Armour controll Recipe change

--- Power-Armour-Replacer/prototypes/recipes/acu_recipes.lua
+++ Power-Armour-Replacer/prototypes/recipes/acu_recipes.lua
@@ -7,8 +7,10 @@ data:extend({
 		enabled = false,
 		ingredients = {
 			{type="item", name="advanced-circuit", amount=4},
-			{type="item", name="plastic-bar", amount=3},
-			{type="item", name="battery", amount=3},
+			{type="item", name="plastic-bar", amount=2},
+			{type="item", name="iron-plate", amount=2},
+			{type="item", name="copper-wire", amount=5},
+			{type="item", name="battery", amount=2},
 		},
 		results = {{type="item", name="armour-control-unit", amount=4}}
 	},
@@ -24,11 +26,12 @@ if mods["space-age"] then
 			energy_required = 10,
 			enabled = false,
 			ingredients = {
-				{type="item", name="tungsten-plate", amount=5},
-				{type="item", name="superconductor", amount=5},
-				{type="item", name="carbon-fiber", amount=5},
+				{type="item", name="quantom-processor", amount=2},
+				{type="item", name="tungsten-plate", amount=8},
+				{type="item", name="superconductor", amount=8},
+				{type="item", name="carbon-fiber", amount=8},
 			},
-			results = {{type="item", name="armour-control-unit", amount=8}}
+			results = {{type="item", name="armour-control-unit", amount=16}}
 		},
 	})
 end
