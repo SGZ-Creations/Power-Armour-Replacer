760dd8590895a8c89ee4352df37164c8bf85b75a
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-11-04 17:47:23 +0100

    Recipe finals?

--- Power-Armour-Replacer/prototypes/recipes/immunity_recipes.lua
+++ Power-Armour-Replacer/prototypes/recipes/immunity_recipes.lua
@@ -1,4 +1,4 @@
-local recipes = {
+data:extend({
     {
         type = "recipe",
         name = "par-belt-immunity-mk1",
@@ -6,10 +6,11 @@ local recipes = {
         enabled = false,
         energy_required = 10,
         ingredients = {
+            {type="item", name="armour-control-unit", amount=200},
+            {type="item", name="advanced-circuit", amount=200},
+            {type="item", name="battery", amount=200},
+            --Fluid
             {type="fluid", name="lubricant", amount=200},
-            {type="item", name="electronic-circuit", amount=30},
-            {type="item", name="steel-plate", amount=55},
-            {type="item", name="battery", amount=5},
         },
         results = {{type="item", name="par-belt-immunity-mk1", amount=1}}
     },
@@ -21,53 +22,67 @@ local recipes = {
         energy_required = 20,
         ingredients = {
             {type="item", name="par-belt-immunity-mk1", amount=1},
-            {type="item", name="low-density-structure", amount=25},
-            {type="item", name="advanced-circuit", amount=55},
-            {type="item", name="processing-unit", amount=40},
-            {type="item", name="nuclear-fuel", amount=10},
+            {type="item", name="armour-control-unit", amount=500},
+            {type="item", name="low-density-structure", amount=50},
+            {type="item", name="processing-unit", amount=60},
+            --Fluid
             {type="fluid", name="sulfuric-acid", amount=150},
         },
         results = {{type="item", name="par-belt-immunity-mk2", amount=1}}
     },
-}
-data:extend(recipes)
+})
 
 if mods["space-age"] then
-    local recipes = {
+    data:extend({
         {
             type = "recipe",
             name = "par-belt-immunity-mk1",
+            main_product = "par-belt-immunity-mk1",
             category = "electronics-or-handcrafting",
             enabled = false,
             energy_required = 10,
             ingredients = {
+                {type="item", name="low-density-structure", amount=250},
+                {type="item", name="armour-control-unit", amount=250},
+                {type="item", name="advanced-circuit", amount=250},
+                {type="item", name="battery", amount=250},
+                {type="item", name="carbon", amount=250},
+                {type="item", name="ice", amount=250},
+                --Fluid
                 {type="fluid", name="lubricant", amount=200},
-                {type="item", name="electronic-circuit", amount=30},
-                {type="item", name="steel-plate", amount=55},
-                {type="item", name="battery", amount=5},
             },
-            results = {{type="item", name="par-belt-immunity-mk1", amount=1}}
+            results = {
+                {type="item", name="par-belt-immunity-mk1", amount=1},
+                {type="fluid", name="water", amount=1000}
+            }
         },
         {
             type = "recipe",
             name = "par-belt-immunity-mk2",
             main_product = "par-belt-immunity-mk2",
-            category = "electronics-or-assembling",
+            category = "electronics",
             enabled = false,
             energy_required = 20,
             ingredients = {
                 {type="item", name="par-belt-immunity-mk1", amount=1},
-                {type="item", name="low-density-structure", amount=25},
-                {type="item", name="advanced-circuit", amount=55},
-                {type="item", name="processing-unit", amount=40},
-                {type="item", name="nuclear-fuel", amount=10},
-                {type="fluid", name="electrolyte", amount=250},
-                {type="fluid", name="petroleum-gas", amount=250},
+                {type="item", name="low-density-structure", amount=500},
+                {type="item", name="armour-control-unit", amount=500},
+                {type="item", name="quantum-processor", amount=500},
+                {type="item", name="tungsten-carbide", amount=500},
+                {type="item", name="tungsten-plate", amount=500},
+                {type="item", name="superconductor", amount=500},
+                {type="item", name="supercapacitor", amount=500},
+                {type="item", name="lithium-plate", amount=500},
+                {type="item", name="holmium-plate", amount=500},
+                {type="item", name="carbon-fiber", amount=500},
+                {type="item", name="biter-egg", amount=500},
+                --Fluid
+                {type="fluid", name="electrolyte", amount=500},
+                {type="fluid", name="ammonia", amount=1000},
             },
             results = {
                 {type="item", name="par-belt-immunity-mk2", amount=1},
             }
         },
-    }
-    data:extend(recipes)
+    })
 end
--- Power-Armour-Replacer/prototypes/recipes/solar_recipes.lua
+++ Power-Armour-Replacer/prototypes/recipes/solar_recipes.lua
@@ -5,10 +5,10 @@ local recipes = {
         enabled = false,
         energy_required = 5,
         ingredients = {
-            {type="item", name="iron-plate", amount=10},
-            {type="item", name="copper-plate", amount=65},
-            {type="item", name="iron-stick", amount=35},
             {type="item", name="electronic-circuit", amount=20},
+            {type="item", name="copper-plate", amount=65},
+            {type="item", name="iron-plate", amount=50},
+            {type="item", name="wood", amount=15},
         },
         results = {{type="item", name="par-solar-panel-mk1", amount=1}}
     },
@@ -19,9 +19,10 @@ local recipes = {
         energy_required = 10,
         ingredients = {
             {type="item", name="par-solar-panel-mk1", amount=1},
+            {type="item", name="electronic-circuit", amount=30},
             {type="item", name="copper-plate", amount=70},
             {type="item", name="steel-plate", amount=50},
-            {type="item", name="electronic-circuit", amount=30},
+            {type="item", name="wood", amount=35},
         },
         results = {{type="item", name="par-solar-panel-mk2", amount=1}}
     },
@@ -32,9 +33,10 @@ local recipes = {
         energy_required = 15,
         ingredients = {
             {type="item", name="par-solar-panel-mk2", amount=1},
-            --{type="item", name="battery", amount=30}, --change this.
-            {type="item", name="steel-plate", amount=75},
+            {type="item", name="advanced-circuit", amount=100},
             {type="item", name="copper-plate", amount=40},
+            {type="item", name="steel-plate", amount=75},
+            {type="item", name="wood", amount=50},
         },
         results = {{type="item", name="par-solar-panel-mk3", amount=1}}
     },
@@ -45,10 +47,9 @@ local recipes = {
         energy_required = 20,
         ingredients = {
             {type="item", name="par-solar-panel-mk3", amount=1},
-            {type="item", name="electronic-circuit", amount=135},
             {type="item", name="advanced-circuit", amount=100},
             {type="item", name="engine-unit", amount=120},
-            --{type="item", name="battery", amount=150},--change this.
+            {type="item", name="battery", amount=150},
         },
         results = {{type="item", name="par-solar-panel-mk4", amount=1}}
     },
@@ -59,10 +60,10 @@ local recipes = {
         energy_required = 25,
         ingredients = {
             {type="item", name="par-solar-panel-mk4", amount=1},
-            --{type="item", name="battery", amount=190}, --change this.
+            {type="item", name="armour-control-unit", amount=80},
             {type="item", name="advanced-circuit", amount=110},
-            {type="item", name="sulfur", amount=123},
             {type="item", name="steel-plate", amount=115},
+            {type="item", name="battery", amount=190},
         },
         results = {{type="item", name="par-solar-panel-mk5", amount=1}}
     },
@@ -73,9 +74,10 @@ local recipes = {
         energy_required = 30,
         ingredients = {
             {type="item", name="par-solar-panel-mk5", amount=1},
+            {type="item", name="armour-control-unit", amount=105},
+            {type="item", name="electric-engine-unit", amount=80},
             {type="item", name="processing-unit", amount=60},
             {type="item", name="engine-unit", amount=140},
-            {type="item", name="electric-engine-unit", amount=80},
             {type="item", name="iron-stick", amount=300},
         },
         results = {{type="item", name="par-solar-panel-mk6", amount=1}}
@@ -87,6 +89,7 @@ local recipes = {
         energy_required = 35,
         ingredients = {
             {type="item", name="par-solar-panel-mk6", amount=1},
+            {type="item", name="armour-control-unit", amount=125},
             {type="item", name="plastic-bar", amount=140},
             {type="item", name="processing-unit", amount=70},
             {type="item", name="electric-engine-unit", amount=120},
@@ -101,8 +104,8 @@ local recipes = {
         energy_required = 40,
         ingredients = {
             {type="item", name="par-solar-panel-mk7", amount=1},
-            {type="item", name="copper-cable", amount=300},
-            --{type="item", name="battery", amount=200}, --change this.
+            {type="item", name="armour-control-unit", amount=155},
+            {type="item", name="battery", amount=200},
             {type="item", name="plastic-bar", amount=170},
             {type="item", name="low-density-structure", amount=175},
         },
@@ -150,10 +153,11 @@ if mods["space-age"] then
             enabled = false,
             energy_required = 5,
             ingredients = {
-                {type="item", name="wood", amount=35},
-                {type="item", name="iron-plate", amount=10},
-                {type="item", name="copper-plate", amount=65},
-                {type="item", name="electronic-circuit", amount=20},
+                {type="item", name="electronic-circuit", amount=25},
+                {type="item", name="copper-plate", amount=25},
+                {type="item", name="steel-plate", amount=25},
+                {type="item", name="iron-plate", amount=25},
+                {type="item", name="wood", amount=25},
             },
             results = {{type="item", name="par-solar-panel-mk1", amount=1}}
         },
@@ -165,9 +169,10 @@ if mods["space-age"] then
             energy_required = 10,
             ingredients = {
                 {type="item", name="par-solar-panel-mk1", amount=1},
-                {type="item", name="electronic-circuit", amount=30},
-                {type="item", name="copper-plate", amount=70},
+                {type="item", name="electronic-circuit", amount=50},
+                {type="item", name="copper-plate", amount=50},
                 {type="item", name="steel-plate", amount=50},
+                {type="item", name="wood", amount=25},
             },
             results = {{type="item", name="par-solar-panel-mk2", amount=1}}
         },
@@ -179,9 +184,10 @@ if mods["space-age"] then
             energy_required = 15,
             ingredients = {
                 {type="item", name="par-solar-panel-mk2", amount=1},
-                {type="item", name="plastic-bar", amount=60},
+                {type="item", name="advanced-circuit", amount=75},
+                {type="item", name="copper-plate", amount=75},
                 {type="item", name="steel-plate", amount=75},
-                {type="item", name="copper-plate", amount=40},
+                {type="item", name="plastic-bar", amount=75},
             },
             results = {{type="item", name="par-solar-panel-mk3", amount=1}}
         },
@@ -193,9 +199,10 @@ if mods["space-age"] then
             energy_required = 20,
             ingredients = {
                 {type="item", name="par-solar-panel-mk3", amount=1},
-                {type="item", name="electronic-circuit", amount=135},
                 {type="item", name="advanced-circuit", amount=100},
-                {type="item", name="plastic-bar", amount=150},
+                {type="item", name="copper-plate", amount=100},
+                {type="item", name="steel-plate", amount=100},
+                {type="item", name="plastic-bar", amount=100},
             },
             results = {{type="item", name="par-solar-panel-mk4", amount=1}}
         },
@@ -207,41 +214,47 @@ if mods["space-age"] then
             energy_required = 25,
             ingredients = {
                 {type="item", name="par-solar-panel-mk4", amount=1},
-                {type="item", name="advanced-circuit", amount=110},
-                {type="item", name="steel-plate", amount=115},
-                {type="item", name="carbon", amount=100},
-                {type="item", name="sulfur", amount=123},
+                {type="item", name="low-density-structure", amount=125},
+                {type="item", name="armour-control-unit", amount=125},
+                {type="item", name="processing-unit", amount=125},
+                {type="item", name="steel-plate", amount=125},
+                {type="item", name="carbon", amount=125},
             },
             results = {{type="item", name="par-solar-panel-mk5", amount=1}}
         },
         {
             type = "recipe",
             name = "par-solar-panel-mk6",
-            category = "electronics-or-assembling",
+            category = "electronics",
             enabled = false,
             energy_required = 30,
             ingredients = {
                 {type="item", name="par-solar-panel-mk5", amount=1},
-                {type="item", name="processing-unit", amount=60},
-                {type="item", name="carbon-fiber", amount=50},
-                {type="item", name="jelly", amount=100},
+                {type="item", name="low-density-structure", amount=150},
+                {type="item", name="armour-control-unit", amount=150},
+                {type="item", name="processing-unit", amount=150},
+                {type="item", name="pentapod-egg", amount=150},
+                {type="item", name="carbon-fiber", amount=150},
+                {type="item", name="carbon", amount=150},
             },
             results = {{type="item", name="par-solar-panel-mk6", amount=1}}
         },
         {
             type = "recipe",
             name = "par-solar-panel-mk7",
-            category = "electronics-or-assembling",
+            category = "electronics",
             enabled = false,
             energy_required = 35,
             ingredients = {
                 {type="item", name="par-solar-panel-mk6", amount=1},
-                {type="item", name="low-density-structure", amount=175},
-                {type="item", name="processing-unit", amount=70},
-                {type="item", name="superconductor", amount=120},
-                {type="item", name="tungsten-carbide", amount=120},
-                {type="item", name="plastic-bar", amount=140},
-                {type="item", name="carbon-fiber", amount=100},
+                {type="item", name="low-density-structure", amount=200},
+                {type="item", name="armour-control-unit", amount=200},
+                {type="item", name="tungsten-carbide", amount=200},
+                {type="item", name="processing-unit", amount=200},
+                {type="item", name="superconductor", amount=200},
+                {type="item", name="carbon-fiber", amount=200},
+                {type="item", name="pentapod-egg", amount=200},
+                --Fluid
                 {type="fluid", name="electrolyte", amount=1000},
             },
             results = {{type="item", name="par-solar-panel-mk7", amount=1}}
@@ -250,16 +263,19 @@ if mods["space-age"] then
             type = "recipe",
             name = "par-solar-panel-mk8",
             main_product = "par-solar-panel-mk8",
-            category = "electronics-or-assembling",
+            category = "electronics",
             enabled = false,
             energy_required = 40,
             ingredients = {
                 {type="item", name="par-solar-panel-mk7", amount=1},
-                {type="item", name="low-density-structure", amount=175},
+                {type="item", name="low-density-structure", amount=300},
+                {type="item", name="armour-control-unit", amount=300},
+                {type="item", name="quantum-processor", amount=300},
+                {type="item", name="supercapacitor", amount=300},
                 {type="item", name="holmium-plate", amount=300},
-                {type="item", name="supercapacitor", amount=150},
-                {type="item", name="carbon-fiber", amount=100},
-                {type="item", name="plastic-bar", amount=170},
+                {type="item", name="carbon-fiber", amount=300},
+                {type="item", name="plastic-bar", amount=300},
+                --Fluid
                 {type="fluid", name="electrolyte", amount=1000},
             },
             results = {{type="item", name="par-solar-panel-mk8", amount=1}}
@@ -268,14 +284,23 @@ if mods["space-age"] then
             type = "recipe",
             name = "par-solar-panel-mk9",
             main_product = "par-solar-panel-mk9",
-            category = "electronics-or-assembling",
+            category = "electronics",
             enabled = false,
             energy_required = 45,
             ingredients = {
                 {type="item", name="par-solar-panel-mk8", amount=1},
-                {type="item", name="processing-unit", amount=123},
-                {type="item", name="armour-control-unit", amount=175},
-                {type="item", name="low-density-structure", amount=175},
+                {type="item", name="low-density-structure", amount=400},
+                {type="item", name="armour-control-unit", amount=400},
+                {type="item", name="quantum-processor", amount=400},
+                {type="item", name="tungsten-carbide", amount=400},
+                {type="item", name="tungsten-plate", amount=400},
+                {type="item", name="superconductor", amount=400},
+                {type="item", name="supercapacitor", amount=400},
+                {type="item", name="lithium-plate", amount=400},
+                {type="item", name="holmium-plate", amount=400},
+                {type="item", name="carbon-fiber", amount=400},
+                {type="item", name="biter-egg", amount=400},
+                --Fluid
                 {type="fluid", name="electrolyte", amount=1000},
                 {type="fluid", name="fluoroketone-cold", amount=100},
             },
@@ -288,14 +313,23 @@ if mods["space-age"] then
             type = "recipe",
             name = "par-solar-panel-mk10",
             main_product = "par-solar-panel-mk10",
-            category = "electronics-or-assembling",
+            category = "electronics",
             enabled = false,
             energy_required = 50,
             ingredients = {
                 {type="item", name="par-solar-panel-mk9", amount=1},
-                {type="item", name="processing-unit", amount=144},
-                {type="item", name="armour-control-unit", amount=200},
-                {type="item", name="low-density-structure", amount=200},
+                {type="item", name="low-density-structure", amount=500},
+                {type="item", name="armour-control-unit", amount=500},
+                {type="item", name="quantum-processor", amount=500},
+                {type="item", name="tungsten-carbide", amount=500},
+                {type="item", name="tungsten-plate", amount=500},
+                {type="item", name="superconductor", amount=500},
+                {type="item", name="supercapacitor", amount=500},
+                {type="item", name="lithium-plate", amount=500},
+                {type="item", name="holmium-plate", amount=500},
+                {type="item", name="carbon-fiber", amount=500},
+                {type="item", name="biter-egg", amount=500},
+                --Fluid
                 {type="fluid", name="electrolyte", amount=1000},
                 {type="fluid", name="fluoroketone-cold", amount=200},
             },
