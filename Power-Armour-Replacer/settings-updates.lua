17330f7fdfec7fc1749f02f19a8dbe302b9bdcae
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-11-29 03:55:20 +0100

    Seettigns sorting Again

--- Power-Armour-Replacer/settings-updates.lua
+++ Power-Armour-Replacer/settings-updates.lua
@@ -1,15 +1,2 @@
-if mods["Power Armor MK3"] then
-	data.raw["bool-setting"]["pam3-nvm2"].hidden = true
-	data.raw["bool-setting"]["pam3-nvm2"].enabled = false
-	data.raw["bool-setting"]["pam3-esm3"].hidden = true
-	data.raw["bool-setting"]["pam3-esm3"].enabled = false
-	data.raw["bool-setting"]["pam3-bm3"].hidden = true
-	data.raw["bool-setting"]["pam3-bm3"].enabled = false
-	data.raw["bool-setting"]["pam3-pam4"].hidden = true
-	data.raw["bool-setting"]["pam3-pam4"].enabled = false
-
-	data.raw["bool-setting"]["pam3-p4xl"].hidden = true
-	data.raw["bool-setting"]["pam3-p4xl"].enabled = false
-	--data.raw["bool-setting"]["pam3-p4gw"].hidden = true
-	--data.raw["bool-setting"]["pam3-p4gh"].hidden = true
-end
+require("Settings.Updates.PowerAromurmK3")
+require("Settings.Updates.ColdBitters")
