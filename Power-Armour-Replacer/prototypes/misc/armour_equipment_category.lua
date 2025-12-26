73229865cbc21e5d2791b6dbbb6ea629e1856454
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-12-26 09:15:20 +0100

    Armour Crafting UI moved order to after combat

--- Power-Armour-Replacer/prototypes/misc/armour_equipment_category.lua
+++ Power-Armour-Replacer/prototypes/misc/armour_equipment_category.lua
@@ -5,7 +5,7 @@ data:extend({
         name = "replacer_armor",
         icon = "__Power-Armour-Replacer__/graphics/icons/armour/icon-power-armour-mk5.png",
         icon_size = 64,
-        order = "ca",
+        order = "ea",
     },
     {
         type = "item-subgroup",
