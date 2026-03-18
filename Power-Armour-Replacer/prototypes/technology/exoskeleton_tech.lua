046093e8e3414db9fb8704ec007219b04f701462
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-03-18 15:29:26 +0100

    Lines

--- Power-Armour-Replacer/prototypes/technology/exoskeleton_tech.lua
+++ Power-Armour-Replacer/prototypes/technology/exoskeleton_tech.lua
@@ -4,14 +4,8 @@ data:extend({
         type = "technology",
         name = "par-exoskeleton-tech-1",
         icons = icon_utils.create_equipment_technology_icon("exoskeleton", 256, 1),
-        prerequisites = { "par-armour-tech-2", "par-fission-reactor-tech-1" },
-        effects =
-        {
-            {
-                type = "unlock-recipe",
-                recipe = "par-exoskeleton-mk1"
-            },
-        },
+        prerequisites = {"par-armour-tech-2", "par-fission-reactor-tech-1" },
+        effects = {{type = "unlock-recipe", recipe = "par-exoskeleton-mk1"}},
         research_trigger = {type ="craft-item", item ="par-armour-mk2", count = 1},
         order = "ExoLegs1"
     },
@@ -20,20 +14,13 @@ data:extend({
         name = "par-exoskeleton-tech-2",
         icons = icon_utils.create_equipment_technology_icon("exoskeleton", 256, 2),
         prerequisites = { "par-exoskeleton-tech-1", "battery", "par-armour-tech-4" },
-        effects =
-        {
-            {
-                type = "unlock-recipe",
-                recipe = "par-exoskeleton-mk2"
-            }
-        },
-        unit =
-        {
+        effects ={{type = "unlock-recipe", recipe = "par-exoskeleton-mk2"}},
+        unit = {
             count = 1000,
             ingredients = {
-                { "automation-science-pack", 1 },
-                { "logistic-science-pack",   1 },
-                { "military-science-pack",   1 },
+                { "automation-science-pack", 1},
+                { "logistic-science-pack",   1},
+                { "military-science-pack",   1},
             },
             time = 15
         },
@@ -44,21 +31,14 @@ data:extend({
         name = "par-exoskeleton-tech-3",
         icons = icon_utils.create_equipment_technology_icon("exoskeleton", 256, 3),
         prerequisites = { "par-exoskeleton-tech-2", "par-armour-tech-6" },
-        effects =
-        {
-            {
-                type = "unlock-recipe",
-                recipe = "par-exoskeleton-mk3"
-            }
-        },
-        unit =
-        {
+        effects = {{ type = "unlock-recipe", recipe = "par-exoskeleton-mk3" }},
+        unit = {
             count = 1500,
             ingredients = {
-                { "automation-science-pack", 1 },
-                { "logistic-science-pack",   1 },
-                { "military-science-pack",   1 },
-                { "chemical-science-pack",   1 },
+                { "automation-science-pack", 1},
+                { "logistic-science-pack",   1},
+                { "military-science-pack",   1},
+                { "chemical-science-pack",   1},
             },
             time = 15
         },
@@ -68,27 +48,16 @@ data:extend({
         type = "technology",
         name = "par-exoskeleton-tech-4",
         icons = icon_utils.create_equipment_technology_icon("exoskeleton", 256, 4),
-        prerequisites =
-        {
-            "par-exoskeleton-tech-3",
-            "par-armour-tech-8",
-        },
-        effects =
-        {
-            {
-                type = "unlock-recipe",
-                recipe = "par-exoskeleton-mk4"
-            }
-        },
-        unit =
-        {
+        prerequisites = { "par-exoskeleton-tech-3", "par-armour-tech-8"},
+        effects ={{type = "unlock-recipe",  recipe = "par-exoskeleton-mk4"}},
+        unit = {
             count = 2000,
             ingredients = {
-                { "automation-science-pack", 1 },
-                { "logistic-science-pack",   1 },
-                { "military-science-pack",   1 },
-                { "chemical-science-pack",   1 },
-                { "production-science-pack", 1 },
+                { "automation-science-pack", 1},
+                { "logistic-science-pack",   1},
+                { "military-science-pack",   1},
+                { "chemical-science-pack",   1},
+                { "production-science-pack", 1},
             },
             time = 15
         },
@@ -99,26 +68,19 @@ data:extend({
         name = "par-exoskeleton-tech-5",
         icons = icon_utils.create_equipment_technology_icon("exoskeleton", 256, 5),
         prerequisites = { "par-exoskeleton-tech-4", "par-armour-tech-10" },
-        effects =
-        {
-            {
-                type = "unlock-recipe",
-                recipe = "par-exoskeleton-mk5"
-            }
-        },
-        unit =
-        {
+        effects = {{type = "unlock-recipe", recipe = "par-exoskeleton-mk5"}},
+        unit = {
             count = 2500,
             ingredients = {
-                { "automation-science-pack", 1 },
-                { "logistic-science-pack",   1 },
-                { "military-science-pack",   1 },
-                { "chemical-science-pack",   1 },
-                { "production-science-pack", 1 },
-                { "utility-science-pack",    1 },
+                { "automation-science-pack", 1},
+                { "logistic-science-pack",   1},
+                { "military-science-pack",   1},
+                { "chemical-science-pack",   1},
+                { "production-science-pack", 1},
+                { "utility-science-pack",    1},
             },
             time = 15
         },
         order = "ExoLegs5"
     }
-});
+})
