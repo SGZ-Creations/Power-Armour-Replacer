acf90f32445c669e4ad370ade39e56dbefa096e7
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-06-24 10:31:31 +0200

    defualting

--- Power-Armour-Replacer/prototypes/items/armour_item.lua
+++ Power-Armour-Replacer/prototypes/items/armour_item.lua
@@ -116,7 +116,7 @@ for tier, equipment in pairs(power_armour) do
         inventory_move_sound = item_sounds.armor_large_inventory_move,
         pick_sound = item_sounds.armor_large_inventory_pickup,
         drop_sound = item_sounds.armor_large_inventory_move,
-        stack_size = 1,--"not-stackable",
+        stack_size = 1,
         weight = equipment.weightvalue,
         equipment_grid = equipment_grid_name, -- disable to removes quality. now make a setting or function that tells if setting =  grids H-W 0 then remove grid entirely. 
         inventory_size_bonus = equipment.inventory_bonus,
