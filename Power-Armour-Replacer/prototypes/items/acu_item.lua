f7518784bda8ad4ab8a7d900ad00c97e19e2f664
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-07-11 11:39:20 +0200

    porting & atemt at fixing bug not reproducable.

--- Power-Armour-Replacer/prototypes/items/acu_item.lua
+++ Power-Armour-Replacer/prototypes/items/acu_item.lua
@@ -18,12 +18,19 @@ data:extend({
 if mods["space-age"] then
 	table.insert(Recipe["scrap-recycling"].results, {type = "item", name = "armour-control-unit", amount = SS["ArmourControlUnit"].value, independent_probability = 0.0001, show_details_in_recipe_tooltip = false})
 end
+
+if mods["recycler"]then
+	data.raw["furnace"]["recycler"].result_inventory_size = 20
+end
+
 if mods["TIMSABA"] then
 	table.insert(Recipe["scrap-recycling"].results, {type = "item", name = "armour-control-unit", amount = SS["ArmourControlUnit"].value, independent_probability = 0.0001, show_details_in_recipe_tooltip = false})
 end
+
 if mods["scrap-reprocessor"]then
 	table.insert(Recipe["sr-electronic-scrap-reprocessing"].results, {type = "item", name = "armour-control-unit", amount = SS["ArmourControlUnit"].value, independent_probability = 0.0015, show_details_in_recipe_tooltip = false})
 end
+
 if mods["space-exploration"] then
 	data.raw["item"]["armour-control-unit"].stack_size = 200
 end
